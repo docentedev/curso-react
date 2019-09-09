bf1f4ce34326b8e7abae8ab206588e96b8694bd5
Claudio Rojas <claudio.dcv@gmail.com>
2019-09-09 18:06:06 -0300

    feat: api-mock v0.1.0

---
+++ api-mock/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require("body-parser");
+
+const db = require("./database/config");
+const userModule = require("./modules/user");
+
+require("./jwt");
+
+const app = express();
+const HTTP_PORT = 3333;
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Start server
+app.listen(HTTP_PORT, () => { console.log(`Runing in port ${HTTP_PORT}`); });
+
+// modules
+userModule(app, db);
